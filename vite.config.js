e1192ffe17b32b6829814e2e8807b25913afd292
razha90 <razhajamsiksyah@gmail.com>
2025-05-08 02:44:28 +0700

    vites

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/moment.js', 'resources/js/app-bootstrap.js'],
             refresh: [`resources/views/**/*`],
         }),
         tailwindcss(),
