39cb8a6662a945fd72ed80e735b94e97bd50ee8a
razha90 <razhajamsiksyah@gmail.com>
2025-05-08 02:48:46 +0700

    vite1

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    base: 'https://fakultas-teknik-production.up.railway.app/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/moment.js', 'resources/js/app-bootstrap.js'],
